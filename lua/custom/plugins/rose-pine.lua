5908e8c9e3615f627f55e128d313fcc3abb8e9c1
Dávid Miklós <david.miklos.work@gmail.com>
2025-12-31 15:37:58 +0200

    feat: style python docstrings as comments in rose-pine theme

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -2,6 +2,12 @@ return {
   'rose-pine/neovim',
   name = 'rose-pine',
   config = function()
+    require('rose-pine').setup {
+      highlight_groups = {
+        ['@string.documentation'] = { link = 'Comment' },
+        ['@string.documentation.python'] = { link = 'Comment' },
+      },
+    }
     vim.cmd.colorscheme 'rose-pine'
   end,
 }
