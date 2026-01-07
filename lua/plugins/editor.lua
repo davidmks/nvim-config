5c59f1228545ae9efb227961f62df2bb75bae7e3
Dávid Miklós <david.miklos.work@gmail.com>
2026-01-07 09:02:08 +0200

    feat: add telescope grep scoped to current directory

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -96,7 +96,17 @@ return {
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sD', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+      vim.keymap.set('n', '<leader>sd', function()
+        local dir
+        if vim.bo.filetype == 'oil' then
+          dir = require('oil').get_current_dir()
+        else
+          dir = vim.fn.expand('%:p:h')
+        end
+        builtin.live_grep { search_dirs = { dir } }
+      end, { desc = '[S]earch grep in [d]irectory' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
