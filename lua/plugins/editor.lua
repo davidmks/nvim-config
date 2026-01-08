c5cb18dd8b8d7f0765ef1a2710fbec67520a99b7
Dávid Miklós <david.miklos.work@gmail.com>
2026-01-08 15:27:12 +0200

    feat: add overseer.nvim for task running

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -42,6 +42,7 @@ return {
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]est' },
+        { '<leader>o', group = '[O]verseer' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -80,6 +80,19 @@ return {
     },
   },
 
+  -- Task runner
+  {
+    'stevearc/overseer.nvim',
+    cmd = { 'OverseerRun', 'OverseerToggle' },
+    keys = {
+      { '<leader>or', '<cmd>OverseerRun<cr>', desc = '[O]verseer [R]un task' },
+      { '<leader>ot', '<cmd>OverseerToggle<cr>', desc = '[O]verseer [T]oggle list' },
+      { '<leader>oa', '<cmd>OverseerTaskAction<cr>', desc = '[O]verseer task [A]ction' },
+      { '<leader>oq', '<cmd>OverseerQuickAction<cr>', desc = '[O]verseer [Q]uick action' },
+    },
+    opts = {},
+  },
+
   -- Linting
   {
     'mfussenegger/nvim-lint',
