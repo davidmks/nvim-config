01b6279cb411c993069ae9b9d49f06cda92233b0
Dávid Miklós <david.miklos.work@gmail.com>
2026-01-13 09:36:16 +0200

    feat(diffview): add keymap to close all diffviews at once

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -6,7 +6,17 @@ return {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff view' },
     { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = '[G]it file [H]istory' },
     { '<leader>gH', '<cmd>DiffviewFileHistory<cr>', desc = '[G]it repo [H]istory' },
-    { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = '[G]it diff [Q]uit' },
+    { '<leader>gQ', '<cmd>DiffviewClose<cr>', desc = '[G]it diff [Q]uit one' },
+    {
+      '<leader>gq',
+      function()
+        local lib = require('diffview.lib')
+        while lib.get_current_view() do
+          vim.cmd('DiffviewClose')
+        end
+      end,
+      desc = '[G]it diff [Q]uit all',
+    },
     { '<leader>gm', '<cmd>DiffviewOpen origin/main...HEAD<cr>', desc = '[G]it diff vs [M]ain' },
     { '<leader>gS', '<cmd>DiffviewOpen --staged<cr>', desc = '[G]it diff [S]taged' },
   },
