c171665ba5f82c62ba1957d83da004aec0331e9b
Dávid Miklós <david.miklos.work@gmail.com>
2026-02-23 11:20:40 +0200

    fix(markdown-preview): use recommended lazy.nvim loading strategy

--- lua/plugins/editor/markdown-preview.lua
+++ lua/plugins/editor/markdown-preview.lua
@@ -1,9 +1,15 @@
 -- Markdown live preview in browser
 return {
   'iamcco/markdown-preview.nvim',
-  ft = 'markdown',
-  build = 'cd app && npm install',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = function()
+    require('lazy').load { plugins = { 'markdown-preview.nvim' } }
+    vim.fn['mkdp#util#install']()
+  end,
   keys = {
-    { '<leader>mp', '<CMD>MarkdownPreviewToggle<CR>', ft = 'markdown', desc = '[M]arkdown [P]review' },
+    { '<leader>Mp', '<CMD>MarkdownPreviewToggle<CR>', ft = 'markdown', desc = '[M]arkdown [P]review' },
   },
+  config = function()
+    vim.cmd [[do FileType]]
+  end,
 }
